e81bc6b89b6acfdec53cd84c1a6dc8cc0517b28b
Dark Dead Dragon <darkdragon1896@gmail.com>
2020-11-20 17:32:40 +0500

    Last try

--- app/src/main/java/com/darkdeaddragon/zaycevtest/MainActivity.kt
+++ app/src/main/java/com/darkdeaddragon/zaycevtest/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.darkdeaddragon.zaycevtest
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val mSharedPref = getSharedPreferences("main",MODE_PRIVATE)
-        var startTimes = mSharedPref.getInt("start_times", 0)
-        val mEditor = mSharedPref.edit()
-        mEditor.putInt("start_times", ++startTimes)
-        mEditor.apply()
-        if (startTimes == 3) Toast.makeText(applicationContext, "Time to feed your turtle!", Toast.LENGTH_SHORT).show()
+        if (savedInstanceState == null) {
+            val mSharedPref = getSharedPreferences("main", MODE_PRIVATE)
+            var startTimes = mSharedPref.getInt("start_times", 0)
+            val mEditor = mSharedPref.edit()
+            mEditor.putInt("start_times", ++startTimes)
+            mEditor.apply()
+            //if (startTimes == 3)
+            Toast.makeText(applicationContext, "Time to feed your $startTimes turtle!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
